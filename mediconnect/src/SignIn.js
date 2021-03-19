592af10b228e3769fbec49857e4c371f1905b72d
dendenxu <56295612@qq.com>
2021-03-19 22:58:41 +0800

    feat: basic signin page

--- mediconnect/src/SignIn.js
+++ mediconnect/src/SignIn.js
@@ -26,35 +26,49 @@ function Copyright() {
   );
 }
 
-const useStyles = makeStyles((theme) => ({
-  paper: {
-    marginTop: theme.spacing(8),
-    display: "flex",
-    flexDirection: "column",
-    alignItems: "center",
-  },
-  avatar: {
-    margin: theme.spacing(1),
-    backgroundColor: theme.palette.secondary.main,
-  },
-  form: {
-    width: "100%", // Fix IE 11 issue.
-    marginTop: theme.spacing(1),
-  },
-  submit: {
-    margin: theme.spacing(3, 0, 2),
-  },
-}));
+const useStyles = makeStyles((theme) => {
+  console.log([`& fieldSet`]);
+  return ({
+    paper: {
+      marginTop: theme.spacing(8),
+      display: "flex",
+      flexDirection: "column",
+      alignItems: "center",
+    },
+    avatar: {
+      margin: theme.spacing(1),
+      backgroundColor: theme.palette.secondary.main,
+    },
+    form: {
+      width: "100%", // Fix IE 11 issue.
+      marginTop: theme.spacing(1),
+    },
+    submit: {
+      margin: theme.spacing(3, 0, 10),
+      display: "flex",
+      flexDirection: "row",
+      justifyContent: "space-between",
+    },
+    welcome: {
+      margin: theme.spacing(3, 0, 3),
+    },
+    input: {
+      ["& fieldset"]: {
+        borderRadius: 20,
+      },
+    },
+  });
+});
 
 export default function SignIn() {
   const classes = useStyles();
   const NextBtn = withStyles({
     root: {
       background: "linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)",
-      borderRadius: "5px",
+      borderRadius: "10px",
       border: 0,
       color: "white",
-      padding: "0 30px",
+      padding: "30 30px",
       boxShadow: "0 3px 5px 2px rgba(255, 105, 135, .3)",
     },
     label: {
@@ -67,7 +81,7 @@ export default function SignIn() {
       <CssBaseline />
       <div className={classes.paper}>
         <Icon />
-        <Typography component="h1" variant="h5">
+        <Typography component="h1" variant="h5" className={classes.welcome}>
           欢迎
         </Typography>
         <TextField
@@ -80,6 +94,7 @@ export default function SignIn() {
           name="email"
           // autoComplete="email"
           autoFocus
+          className={classes.input}
         />
         <Container>
           <FormControlLabel
@@ -88,38 +103,29 @@ export default function SignIn() {
           />
         </Container>
 
-        <Container
-          style={{
-            display: "flex",
-            flexDirection: "row",
-            justifyContent: "space-between",
-          }}
-        >
+        <Container className={classes.submit}>
           <Link href="https://neon-cubes.xyz" variant="body2">
             忘记了密码？
           </Link>
-          <NextBtn
-            type="submit"
-            variant="contained"
-            color="primary"
-            className={classes.submit}
-          >
+          <NextBtn type="submit" variant="contained" color="primary">
             下一步
           </NextBtn>
         </Container>
 
-        <Grid container>
-          <Grid item xs>
-            <Link href="#" variant="body2">
-              忘记了密码？
-            </Link>
+        <Container>
+          <Grid container>
+            <Grid item xs>
+              <Link href="#" variant="body2">
+                忘记了密码？
+              </Link>
+            </Grid>
+            <Grid item>
+              <Link href="#" variant="body2">
+                {"没有账号？注册一个！"}
+              </Link>
+            </Grid>
           </Grid>
-          <Grid item>
-            <Link href="#" variant="body2">
-              {"Don't have an account? Sign Up"}
-            </Link>
-          </Grid>
-        </Grid>
+        </Container>
       </div>
       <Box mt={8}>
         <Copyright />
