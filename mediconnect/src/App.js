8f86d959e412cf2615945f0dde6af1a48ef37b23
徐震 <xuzhen@dailyinnovation.biz>
2021-03-19 12:52:44 +0800

    feat: align typography

--- mediconnect/src/App.js
+++ mediconnect/src/App.js
@@ -1,9 +1,11 @@
 import React from "react";
 import { Grid, Typography } from "@material-ui/core";
-import CoolButton from "./CoolButton"
+
 export default function App() {
   const cool = false
   return (
-    <CoolButton cool={cool}></CoolButton>
+    <Typography align="right">
+      Hello, world
+    </Typography>
   );
 }
