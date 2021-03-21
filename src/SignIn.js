88bea2efa502a858c193c202c3cf62c5878a4eb7
dendenxu <56295612@qq.com>
2021-03-21 13:18:54 +0800

    feat: 完善登陆页面，并更新主题

--- src/SignIn.js
+++ src/SignIn.js
@@ -12,19 +12,7 @@ import { ReactComponent as Icon } from "./Icon.svg";
 import Typography from "@material-ui/core/Typography";
 import { makeStyles, withStyles } from "@material-ui/core/styles";
 import Container from "@material-ui/core/Container";
-
-function Copyright() {
-  return (
-    <Typography variant="body2" color="textSecondary" align="center">
-      {"Copyright © "}
-      <Link color="inherit" href="https://github.com/dendenxu">
-        dendenxu
-      </Link>{" "}
-      {new Date().getFullYear()}
-      {"."}
-    </Typography>
-  );
-}
+import { Radio } from "@material-ui/core";
 
 const useStyles = makeStyles((theme) => {
   return {
@@ -49,23 +37,95 @@ const useStyles = makeStyles((theme) => {
       justifyContent: "space-between",
     },
     welcome: {
-      margin: theme.spacing(3, 0, 3),
+      margin: theme.spacing(2, 0, 1),
     },
     input: {
       "& fieldset": {
-        borderRadius: 20,
+        borderRadius: 16,
       },
+      padding: theme.spacing(0, 1),
+      margin: theme.spacing(0, 0, -0.5),
     },
     nextButton: {
-      background: "linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)",
+      // background: "linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)",
       borderRadius: "10px",
       border: 0,
       color: "white",
       padding: "30 30px",
-      boxShadow: "0 3px 5px 2px rgba(255, 105, 135, .3)",
+      // boxShadow: "0 3px 5px 2px rgba(255, 105, 135, .3)",
+    },
+    avatarButton: {
+      borderRadius: "14px",
+      textTransform: "none",
+      marginBottom: theme.spacing(2),
+    },
+    smallAvatar: {
+      width: theme.spacing(2),
+      height: theme.spacing(2),
+    },
+    centeredText: {
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+      // ! special operation for Josefin Sans
+      transform: "translate(0px,1.5px)"
     },
   };
 });
+
+function Copyright() {
+  return (
+    <Typography variant="body2" color="textSecondary" align="center">
+      {"Copyright © "}
+      <Link color="inherit" href="https://github.com/dendenxu">
+        dendenxu
+      </Link>{" "}
+      {new Date().getFullYear()}
+      {"."}
+    </Typography>
+  );
+}
+
+const BottomBar = (props) => {
+  const { name } = props;
+  return (
+    <Container>
+      <Grid container spacing={2}>
+        <Grid item xs>
+          <Link href="#" variant="body2">
+            {name === "" ? "帮助" : `Got: ${name}`}
+          </Link>
+        </Grid>
+        <Grid item>
+          <Link href="#" variant="body2">
+            {"使用条款"}
+          </Link>
+        </Grid>
+        <Grid item>
+          <Link href="#" variant="body2">
+            {"隐私协议"}
+          </Link>
+        </Grid>
+      </Grid>
+    </Container>
+  );
+};
+
+const AvatarBar = (props) => {
+  const { email, avatarSrc, handleAvatarClick } = props;
+  const classes = useStyles();
+  return (
+    <Button
+      variant="outlined"
+      size="small"
+      className={classes.avatarButton}
+      startIcon={<Avatar src={avatarSrc} className={classes.smallAvatar} />}
+      onClick={handleAvatarClick}
+    >
+      {email}
+    </Button>
+  );
+};
 
 export default function SignIn() {
   const classes = useStyles();
@@ -87,6 +147,9 @@ export default function SignIn() {
     setName(newName);
     console.log(`Getting new name: ${newName}`);
   };
+  const handleAvatarClick = () => {
+    console.log("Avatar Clicked!");
+  };
   return (
     <Container component="main" maxWidth="xs">
       <CssBaseline />
@@ -95,29 +158,56 @@ export default function SignIn() {
         <Typography component="h1" variant="h5" className={classes.welcome}>
           欢迎
         </Typography>
-        <TextField
-          variant="outlined"
-          margin="normal"
-          fullWidth
-          id="password"
-          label="输入您的密码"
-          name="password"
-          autoFocus
-          className={classes.input}
-          value={name}
-          onChange={handleNameChange}
+        <AvatarBar
+          email="3180105504@zju.edu.cn"
+          avatarSrc="https://courses.zju.edu.cn/api/uploads/2232880/modified-image"
+          handleAvatarClick={handleAvatarClick}
         />
+        <Container className={classes.input}>
+          <TextField
+            variant="outlined"
+            fullWidth
+            id="password"
+            label="输入您的密码"
+            name="password"
+            autoFocus
+            value={name}
+            onChange={handleNameChange}
+          />
+        </Container>
+
         <Container>
           <FormControlLabel
-            control={<Checkbox value="remember" color="primary" />}
-            label="显示密码"
+            control={
+              <Checkbox
+                value="remember"
+                color="primary"
+                size="small"
+
+              />
+            }
+            label={
+              <Typography
+                className={classes.centeredText}
+                variant="body2"
+                style={{
+                  marginLeft: -5
+                }}
+              >
+                显示密码
+              </Typography>
+            }
             checked={selected}
             onChange={handleChange}
           />
         </Container>
 
         <Container className={classes.submit}>
-          <Link href="https://neon-cubes.xyz" variant="body2">
+          <Link
+            href="https://neon-cubes.xyz"
+            variant="body2"
+            className={classes.centeredText}
+          >
             {clicked ? "你点我！" : "忘记了密码?"}
           </Link>
           <Button
@@ -131,20 +221,7 @@ export default function SignIn() {
           </Button>
         </Container>
 
-        <Container>
-          <Grid container>
-            <Grid item xs>
-              <Link href="#" variant="body2">
-                {name === "" ? "忘记了密码？" : `Got: ${name}`}
-              </Link>
-            </Grid>
-            <Grid item>
-              <Link href="#" variant="body2">
-                {"没有账号？注册一个！"}
-              </Link>
-            </Grid>
-          </Grid>
-        </Container>
+        <BottomBar name={name} />
       </div>
       <Box mt={8}>
         <Copyright />
