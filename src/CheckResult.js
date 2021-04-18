ee92d84bf8cefebd08e242d05aa0dd6f167b5fa8
ZimuSama <3229329616@qq.com>
2021-04-18 13:11:05 +0800

    Add CheckResult.js result.js and some needed img

---
+++ src/CheckResult.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import { BrowserRouter as Router, Link, Route, Switch } from 'react-router-dom';
+import './index.css';
+import { Box, Button } from '@material-ui/core';
+import Result from './result';
+class App extends React.Component {
+    render() {
+        return (
+            <Router>
+                <Switch>
+                    <Route exact path="/">
+                        <Box
+                            display="flex"
+                            bgcolor="lightblue"
+                            height={window.innerHeight}>
+                            <Box m="auto">
+                                <Link
+                                    to={{
+                                        pathname: '/result',
+                                        state: {  // 页面跳转要传递的数据，如下
+                                            /*
+                                            data1: {
+                                                doc: '子沐',
+                                                name: '子曰',
+                                                dep: '推拿房',
+                                                year: 1999,
+                                                mon: 1,
+                                                day: 1,
+                                                tim: 0
+                                            }*/
+                                            data1:{
+                                                reason:'当天无可安排医生'
+                                            }
+                                        },
+                                    }}
+                                    style={{ textDecoration: 'none' }}>
+                                    <Button variant="contained" color="primary">
+                                        挂号
+                                    </Button>
+                                </Link>
+                            </Box>
+                        </Box>
+                    </Route>
+                    <Route path="/result" component={Result}></Route>
+                </Switch>
+            </Router>
+        );
+    }
+}
+export default App;
