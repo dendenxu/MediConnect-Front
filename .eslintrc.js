dda879741aebf5f1b5032fd59ec898de962ae1a1
dendenxu <56295612@qq.com>
2021-04-20 21:38:10 +0800

    fix: eslint and prettier conflicts

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,7 @@ module.exports = {
     browser: true,
     es2021: true,
   },
-  extends: ['plugin:react/recommended', 'airbnb'],
+  extends: ['airbnb', 'prettier', 'plugin:react/recommended'],
   parserOptions: {
     ecmaFeatures: {
       jsx: true,
