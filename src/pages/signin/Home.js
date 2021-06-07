1e64c8e29bc04d9546b59621eaa71ff6bb5b9aa2
BlossomWay <2485280111@qq.com>
2021-06-08 00:04:16 +0800

    Update Home.js

--- src/pages/signin/Home.js
+++ src/pages/signin/Home.js
@@ -1,146 +1,152 @@
-/* eslint-disable react/prop-types */
-/* eslint-disable jsx-a11y/anchor-is-valid */
-import React, { useState } from 'react';
-import { useHistory } from 'react-router-dom';
-import Avatar from '@material-ui/core/Avatar';
-import Button from '@material-ui/core/Button';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import TextField from '@material-ui/core/TextField';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Checkbox from '@material-ui/core/Checkbox';
-import Link from '@material-ui/core/Link';
-import Grid from '@material-ui/core/Grid';
-import Box from '@material-ui/core/Box';
-import Typography from '@material-ui/core/Typography';
-import { makeStyles } from '@material-ui/core/styles';
-import Container from '@material-ui/core/Container';
-import { ReactComponent as Icon } from '../../assets/images/Icon.svg';
+import React, { useState } from "react";
+import { useHistory } from "react-router-dom";
+import { createBrowserHistory } from "history";
+import Avatar from "@material-ui/core/Avatar";
+import Button from "@material-ui/core/Button";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import TextField from "@material-ui/core/TextField";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+import Checkbox from "@material-ui/core/Checkbox";
+import Link from "@material-ui/core/Link";
+import Grid from "@material-ui/core/Grid";
+import Box from "@material-ui/core/Box";
+import { ReactComponent as Icon } from "./Icon.svg";
+import Typography from "@material-ui/core/Typography";
+import { makeStyles, withStyles } from "@material-ui/core/styles";
+import Container from "@material-ui/core/Container";
+import { Radio } from "@material-ui/core";
+import { render } from "@testing-library/react";
+import { red } from "@material-ui/core/colors";
+import SignIn from "./SignIn";
 
-const useStyles = makeStyles(theme => ({
-  borderedContainer: {
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
-    border: 5,
-    borderRadius: 30,
-    boxShadow: '0 0px 5px 1px rgba(33, 33, 33, .3)',
-    padding: theme.spacing(3),
-    width: '90%',
-  },
-  paper: {
-    marginTop: theme.spacing(8),
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
-  },
-  avatar: {
-    margin: theme.spacing(1),
-    backgroundColor: theme.palette.secondary.main,
-  },
-  form: {
-    width: '100%', // Fix IE 11 issue.
-    marginTop: theme.spacing(1),
-  },
-  submit: {
-    margin: theme.spacing(5.5, 0, 5.5),
-    display: 'flex',
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-  },
-  welcome: {
-    margin: theme.spacing(1, 0, 1),
-    color: theme.palette.secondary.main,
-  },
-  checkboxInput: {
-    padding: theme.spacing(0),
-    margin: theme.spacing(1, 0, -0.5),
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
-    width: '85%',
-  },
-
-  input: {
-    // borderRadius: 12,
-    // "& input": {
-    //   borderRadius: 12,
-    // },
-    '& div': {
-      borderRadius: 12,
+const useStyles = makeStyles((theme) => {
+  return {
+    borderedContainer: {
+      display: "flex",
+      flexDirection: "column",
+      alignItems: "center",
+      border: 5,
+      borderRadius: 30,
+      boxShadow: "0 0px 5px 1px rgba(33, 33, 33, .3)",
+      padding: theme.spacing(3),
+      width: "90%",
     },
-    // "& fieldset": {
-    //   borderRadius: 12,
-    // },
-  },
-  checkboxContainer: {
-    display: 'flex',
-    justifyContent: 'flex-end',
-    // width: "100%"
-    marginTop: theme.spacing(-0.5),
-    padding: 0,
-  },
-  nextButton: {
-    // background: "linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)",
-    borderRadius: '10px',
-    border: 0,
-    color: 'white',
-    padding: '30 30px',
-    // boxShadow: "0 3px 5px 2px rgba(255, 105, 135, .3)",
-  },
-  avatarButton: {
-    borderRadius: '14px',
-    textTransform: 'none',
-    marginBottom: theme.spacing(2),
-    padding: '-0px 5px',
-  },
-  smallAvatar: {
-    width: theme.spacing(2),
-    height: theme.spacing(2),
-  },
-  centeredText: {
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'center',
-    // ! special operation for Josefin Sans
-    transform: 'translate(0px,1.5px)',
-  },
-  icon: {
-    width: '100%',
-    height: '100%',
-    marginBottom: theme.spacing(3),
-  },
-}));
+    paper: {
+      marginTop: theme.spacing(8),
+      display: "flex",
+      flexDirection: "column",
+      alignItems: "center",
+    },
+    avatar: {
+      margin: theme.spacing(1),
+      backgroundColor: theme.palette.secondary.main,
+    },
+    form: {
+      width: "100%", // Fix IE 11 issue.
+      marginTop: theme.spacing(1),
+    },
+    submit: {
+      margin: theme.spacing(5.5, 0, 5.5),
+      display: "flex",
+      flexDirection: "row",
+      justifyContent: "space-between",
+    },
+    welcome: {
+      margin: theme.spacing(1, 0, 1),
+      color: theme.palette.secondary.main,
+    },
+    checkboxInput: {
+      padding: theme.spacing(0),
+      margin: theme.spacing(1, 0, -0.5),
+      display: "flex",
+      flexDirection: "column",
+      alignItems: "center",
+      width: "85%",
+    },
+
+    input: {
+      // borderRadius: 12,
+      // "& input": {
+      //   borderRadius: 12,
+      // },
+      "& div": {
+        borderRadius: 12,
+      },
+      // "& fieldset": {
+      //   borderRadius: 12,
+      // },
+    },
+    checkboxContainer: {
+      display: "flex",
+      justifyContent: "flex-end",
+      // width: "100%"
+      marginTop: theme.spacing(-0.5),
+      padding: 0,
+    },
+    nextButton: {
+      // background: "linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)",
+      borderRadius: "10px",
+      border: 0,
+      color: "white",
+      padding: "30 30px",
+      // boxShadow: "0 3px 5px 2px rgba(255, 105, 135, .3)",
+    },
+    avatarButton: {
+      borderRadius: "14px",
+      textTransform: "none",
+      marginBottom: theme.spacing(2),
+      padding: "-0px 5px",
+    },
+    smallAvatar: {
+      width: theme.spacing(2),
+      height: theme.spacing(2),
+    },
+    centeredText: {
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+      // ! special operation for Josefin Sans
+      transform: "translate(0px,1.5px)",
+    },
+    icon: {
+      width: "100%",
+      height: "100%",
+      marginBottom: theme.spacing(3),
+    },
+  };
+});
 
 function Copyright() {
   return (
     <Typography variant="caption" color="textSecondary" align="center">
-      {'Copyright © '}
+      {"Copyright © "}
       <Link color="inherit" href="https://github.com/dendenxu">
         dendenxu
-      </Link>{' '}
-      {new Date().getFullYear()}.
+      </Link>{" "}
+      {new Date().getFullYear()}
+      {"."}
     </Typography>
   );
 }
 
-const BottomBar = props => {
+const BottomBar = (props) => {
   const { name } = props;
   return (
     <Container>
       <Grid container spacing={2}>
         <Grid item xs>
           <Link color="textSecondary" href="#" variant="caption">
-            {name === '' ? '帮助' : `Got: ${name}`}
+            {name === "" ? "帮助" : `Got: ${name}`}
           </Link>
         </Grid>
         <Grid item>
           <Link color="textSecondary" href="#" variant="caption">
-            使用条款
+            {"使用条款"}
           </Link>
         </Grid>
         <Grid item>
           <Link color="textSecondary" href="#" variant="caption">
-            隐私协议
+            {"隐私协议"}
           </Link>
         </Grid>
       </Grid>
@@ -148,7 +154,7 @@ const BottomBar = props => {
   );
 };
 
-const AvatarBar = props => {
+const AvatarBar = (props) => {
   const { email, avatarSrc, handleAvatarClick } = props;
   const classes = useStyles();
   return (
@@ -170,64 +176,57 @@ export default function Home() {
   const [emailCheck, setEmailCheck] = useState(false);
   const [clicked, setClicked] = useState(false);
   const [selected, setSelected] = useState(false);
-  const [name, setName] = useState('');
-
-  const history = useHistory();
-
-  const handleClick = event => {
+  const [name, setName] = useState("");
+  const [name1, setName1]= useState("");
+  const handleClick = (event) => {
     const newVal = !clicked;
     setClicked(newVal);
     console.log(`clicked: ${newVal}`);
-    console.log(
-      `Getting new email: ${name}, enter 3180105504@zju.edu.cn to get a preview`,
-    );
-    console.log(event);
-    if (name === '3180105504@zju.edu.cn') {
-      console.log('Setting emailCheck to true');
-      setEmailCheck(true);
-    }
-    // const username = document.getElementById("username").value;
-    // console.log({ username: username });
-    // let history = createBrowserHistory();
-    // if (username == 123456) {
-    // history.push("/SignIn");
-    // history.go();
-    // }
-  };
-  const handleChange = event => {
+    const username = document.getElementById("username").value;
+    document.getElementById('username').value = '';
+    //console.log(
+    //`Getting new email: ${name}, enter 3180105504@zju.edu.cn to get a preview`
+    //);
+    // console.log(event);
+    //if (name === "3180105504@zju.edu.cn") {
+    //}
+    setEmailCheck(true);
+    setName("")
+    fetch('url', {//url：服务器地址
+      method: "post",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({//请求的参数
+        username: username,
+      })
+    }).then(res => {
+      return res.text().then((response) => {
+        console.log(response);
+        console.log(emailCheck)
+        setEmailCheck(true);
+      });
+    })
+    .catch(err=>{console.log("账号不存在",err)})
+  }
+    
+  const handleChange = (event) => {
     const sel = event.target.checked;
     setSelected(sel);
     console.log(`selected: ${sel}`);
   };
 
-  const handleNameChange = event => {
+  const handleNameChange = (event) => {
     const newName = event.target.value;
     setName(newName);
     console.log(`Getting new name: ${newName}`);
+    if (!emailCheck) {
+      setName1(newName)
+    }    
   };
   const handleAvatarClick = () => {
-    console.log('Avatar Clicked!');
+    console.log("Avatar Clicked!");
   };
-  const handleSignUpClick = () => {
-    const location = {
-      pathname: '/result',
-      state: {
-        // 页面跳转要传递的数据，如下
-
-        data1: {
-          doc: '子沐',
-          name,
-          dep: '推拿房',
-          year: 1999,
-          mon: 1,
-          day: 1,
-          tim: 0,
-        },
-      },
-    };
-    history.push(location);
-  };
-
   return (
     <Container component="main" maxWidth="xs">
       <CssBaseline />
@@ -236,11 +235,11 @@ export default function Home() {
 
         <Box className={classes.borderedContainer}>
           <Typography component="h1" variant="h5" className={classes.welcome}>
-            {emailCheck ? '欢迎' : '登录'}
+            {emailCheck ? "欢迎" : "登录"}
           </Typography>
           {emailCheck ? (
             <AvatarBar
-              email="3180105504@zju.edu.cn"
+              email={name1}
               avatarSrc="https://courses.zju.edu.cn/api/uploads/2232880/modified-image"
               handleAvatarClick={handleAvatarClick}
             />
@@ -251,7 +250,7 @@ export default function Home() {
               className={classes.welcome}
             >
               使用您的 MediConnect 账号
-            </Typography>
+             </Typography>
           )}
 
           <Container className={classes.checkboxInput}>
@@ -260,13 +259,14 @@ export default function Home() {
               variant="outlined"
               size="small"
               id="username"
-              label="邮箱账号"
+              label={!emailCheck ? "邮箱账号" : "输入密码"}
               name="username"
               autoFocus
               fullWidth
               value={name}
               onChange={handleNameChange}
-            />
+              
+            ></TextField>
 
             {emailCheck ? (
               <Container className={classes.checkboxContainer}>
@@ -283,7 +283,7 @@ export default function Home() {
                       }}
                     >
                       显示密码
-                    </Typography>
+                     </Typography>
                   }
                   checked={selected}
                   onChange={handleChange}
@@ -293,18 +293,18 @@ export default function Home() {
                 />
               </Container>
             ) : (
-              <Container />
+              <Container></Container>
             )}
           </Container>
 
           <Container className={classes.submit}>
             <Link
+              href="https://neon-cubes.xyz"
               variant="caption"
               className={classes.centeredText}
-              onClick={handleSignUpClick}
             >
               创建新账号
-            </Link>
+             </Link>
             <Button
               className={classes.nextButton}
               type="submit"
@@ -313,7 +313,7 @@ export default function Home() {
               onClick={handleClick}
             >
               下一步
-            </Button>
+             </Button>
           </Container>
           <BottomBar name={name} />
         </Box>
