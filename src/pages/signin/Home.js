75c8b3d724c16bb51a3a7bafb3b764f7e460b0c2
BlossomWay <2485280111@qq.com>
2021-06-08 00:11:12 +0800

    Update Home.js

--- src/pages/signin/Home.js
+++ src/pages/signin/Home.js
@@ -1,6 +1,4 @@
 import React, { useState } from "react";
-import { useHistory } from "react-router-dom";
-import { createBrowserHistory } from "history";
 import Avatar from "@material-ui/core/Avatar";
 import Button from "@material-ui/core/Button";
 import CssBaseline from "@material-ui/core/CssBaseline";
