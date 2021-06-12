5d9076121630dcede7d186bb61edd16e1848d5c0
dendenxu <56295612@qq.com>
2021-06-12 22:46:23 +0800

    fix: npm finally showing up pages

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import { createBrowserHistory } from 'history';
 import Home from './pages/signin/Home';
+import Signup from './pages/signup/Signup';
 import Result from './pages/process/Result';
 import Chat from './pages/chat/Chat';
 
@@ -10,6 +11,7 @@ export default function App() {
     <Router history={createBrowserHistory()}>
       <Route exact path="/" component={Home} />
       <Route exact path="/Result" component={Result} />
+      <Route exact path="/Signup" component={Signup} />
       <Route exact path="/Chat" component={Chat} />
     </Router>
   );
