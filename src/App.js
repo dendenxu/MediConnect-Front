eac768f4a79c3b769ff04e49b3ccc5ba147f1897
JudyZ <1172431325@qq.com>
2021-06-17 20:47:56 +0800

    feat: update patient Chat

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Home from './pages/signin/Home';
 import Signup from './pages/signup/Signup';
 import Result from './pages/process/Result';
 import Chat from './pages/chat/Chat';
+import ChatPatient from './pages/chat/ChatPatient';
 
 export default function App() {
   return (
@@ -13,6 +14,7 @@ export default function App() {
       <Route exact path="/Result" component={Result} />
       <Route exact path="/Signup" component={Signup} />
       <Route exact path="/Chat" component={Chat} />
+      <Route exact path="/ChatPatient" component={ChatPatient} />
     </Router>
   );
 }
