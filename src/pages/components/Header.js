7221947b2aa1ecf6799138402e2322386de6ee05
Ziyue-PAN <ziyuepan99@outlook.com>
2021-06-19 14:44:59 +0800

    add goback for header

--- src/pages/components/Header.js
+++ src/pages/components/Header.js
@@ -1,10 +1,19 @@
-import { AppBar, Toolbar, makeStyles, Typography } from '@material-ui/core';
+import {
+  AppBar,
+  Toolbar,
+  makeStyles,
+  Typography,
+  IconButton,
+} from '@material-ui/core';
 import { withStyles } from '@material-ui/core/styles';
+import { useHistory } from 'react-router-dom';
+import ArrowBackIosIcon from '@material-ui/icons/ArrowBackIos';
 import React from 'react';
 
 const useStyles = makeStyles(theme => ({
   header: {
     backgroundColor: theme.palette.primary.main,
+    width: '100%',
   },
 }));
 
@@ -17,19 +26,31 @@ const WhiteTextTypography = withStyles({
 export default function Header(props) {
   const { header } = useStyles();
 
+  const history = useHistory();
+
   const { text } = props;
 
   const HeaderText = (
-    <WhiteTextTypography variant="h5" component="h1">
-      {text}
-    </WhiteTextTypography>
+    <WhiteTextTypography variant="h6">{text}</WhiteTextTypography>
   );
 
   return (
-    <header>
-      <AppBar position="sticky" className={header}>
-        <Toolbar>{HeaderText}</Toolbar>
-      </AppBar>
-    </header>
+    <AppBar position="sticky" className={header}>
+      <Toolbar>
+        <IconButton
+          onClick={() => {
+            history.goBack();
+          }}
+          size="small"
+          edge="start"
+          style={{
+            color: '#FFFFFF',
+          }}
+        >
+          <ArrowBackIosIcon fontSize="small" />
+        </IconButton>
+        {HeaderText}
+      </Toolbar>
+    </AppBar>
   );
 }
