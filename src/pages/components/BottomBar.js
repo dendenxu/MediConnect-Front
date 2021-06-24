69f2345ef80a3b749b4375242fd36b030d0eaf07
yyt <2238275317@qq.com>
2021-06-25 00:25:38 +0800

    feat: add help description

--- src/pages/components/BottomBar.js
+++ src/pages/components/BottomBar.js
@@ -7,17 +7,17 @@ export default function BottomBar(props) {
   return (
     <Grid container spacing={2} className={className}>
       <Grid item xs={spaceOut}>
-        <Link color="textSecondary" href="neon-cubes.xyz" variant="caption">
+        <Link color="textSecondary" href="/help/Help.js" variant="caption">
           帮助
         </Link>
       </Grid>
       <Grid item>
-        <Link color="textSecondary" href="neon-cubes.xyz" variant="caption">
+        <Link color="textSecondary" href="/help/Help.js" variant="caption">
           使用条款
         </Link>
       </Grid>
       <Grid item>
-        <Link color="textSecondary" href="neon-cubes.xyz" variant="caption">
+        <Link color="textSecondary" href="/help/Help.js" variant="caption">
           隐私协议
         </Link>
       </Grid>
