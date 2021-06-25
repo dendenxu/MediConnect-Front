a1b760f6761903cd427be5464ccf7282c16fceb1
BlossomWay <2485280111@qq.com>
2021-06-25 11:57:10 +0800

    feat:个人主页加载时姓名和邮箱的显示
    onLoad事件不触发，界面加载的时候没有显示从后端获取到的姓名和邮箱，改成onclick事件后，需要点击一下页面姓名和邮箱正确显示。onLoad事件不触发的原因未知

--- src/pages/signin/Doctor.js
+++ src/pages/signin/Doctor.js
@@ -3,11 +3,16 @@ import { useHistory, useLocation } from 'react-router-dom';
 import Button from '@material-ui/core/Button';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import TextField from '@material-ui/core/TextField';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
+import Link from '@material-ui/core/Link';
+import Grid from '@material-ui/core/Grid';
 import Box from '@material-ui/core/Box';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
 import PersonOutlineIcon from '@material-ui/icons/PersonOutline';
+import withWidth, { isWidthUp, isWidthDown } from '@material-ui/core/withWidth';
 import { ReactComponent as Icon } from '../../assets/images/icon.svg';
 
 const useStyles = makeStyles(theme => ({
@@ -36,31 +41,26 @@ const useStyles = makeStyles(theme => ({
 export default function Doctor() {
   const classes = useStyles();
   const history = useHistory();
-  const [name, setName] = useState('default value');
-  const [email, setEmail] = useState('default value');
+  const [myName, setName] = useState('default value');
+  const [myEmail, setEmail] = useState('default value');
   const [introduction, setIntr] = useState('default value');
   const [inputContent, setInputContent] = useState('');
   const getInfo = async () => {
-    const response = await fetch('/api/account/getinfo', {
-      method: 'get',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(),
-    });
+    const response = await fetch('/api/account/getinfo');
+    const body = await response.json();
     console.log(response);
     if (response.ok) {
-      const body = await response.json();
       console.log(body);
-      setName(body.firstName + body.lastName);
-      setEmail(body.email);
+      setName(body.lastName + body.firstName);
+      setEmail(body.myEmail);
     }
-  }
+  };
   const handleEditPWDButtonClick = event => {
     history.push({
       pathname: '/editpass',
       state: {
-        afterEmailCheck: 'true',
+        email: myEmail,
+        homepage:true
       }
     });
   };
@@ -77,36 +77,77 @@ export default function Doctor() {
 
   // （待完成）调取病人的病例并显示
   return (
-    <Container component="main" className={classes.page}>
+    <Container component="main" className={classes.page} onClick={getInfo}>
       <CssBaseline />
       <Box className={classes.start} bgcolor="#87CEFA" p={1}>
-        <Box><Icon /></Box>
-        <Box><PersonOutlineIcon /></Box>
-        <Box><Typography>个人资料</Typography></Box>
+        <Box>
+          <Icon />
+        </Box>
+        <Box>
+          <PersonOutlineIcon />
+        </Box>
+        <Box>
+          <Typography>个人资料</Typography>
+        </Box>
       </Box>
-      <Box className={classes.start} p={1} color="primary.main" borderBottom={5}>
+      <Box
+        className={classes.start}
+        p={1}
+        color="primary.main"
+        borderBottom={5}
+      >
         <Typography>基本信息</Typography>
       </Box>
-      <Box className={classes.center} p={1} borderBottom={5} borderColor="primary.main">
-        <Box display='flex' width='33%'>
-          <Typography>姓名：{name}</Typography>
+      <Box
+        className={classes.center}
+        p={1}
+        borderBottom={5}
+        borderColor="primary.main"
+        onLoad={getInfo}
+      >
+        <Box display="flex" width="33%">
+          <Typography>姓名：{myName}</Typography>
         </Box>
-        <Box display='flex' width='33%'>
-          <Typography>邮箱：{email}</Typography>
+        <Box display="flex" width="33%">
+          <Typography>邮箱：{myEmail}</Typography>
         </Box>
-        <Box display='flex' width='33%'>
-          <Button variant="contained" color="primary" onClick={handleEditPWDButtonClick}>修改密码</Button>
+        <Box display="flex" width="33%">
+          <Button
+            variant="contained"
+            color="primary"
+            onClick={handleEditPWDButtonClick}
+          >
+            修改密码
+          </Button>
         </Box>
       </Box>
       <Box className={classes.start} p={1}>
-        <Box><Typography>个人介绍:</Typography></Box>
+        <Box>
+          <Typography>个人介绍:</Typography>
+        </Box>
       </Box>
-      <Box className={classes.start} p={1} borderColor="primary.main" borderBottom={5}>
-        <TextField value={introduction} onChange={handleInputChange} id="introduction" />
+      <Box
+        className={classes.start}
+        p={1}
+        borderColor="primary.main"
+        borderBottom={5}
+      >
+        <TextField
+          value={introduction}
+          onChange={handleInputChange}
+          id="introduction"
+        />
       </Box>
-      <Box className={classes.start} p={1} Color="primary.main" borderBottom={5}>
-        <Box><Typography>病人病历</Typography></Box>
+      <Box
+        className={classes.start}
+        p={1}
+        Color="primary.main"
+        borderBottom={5}
+      >
+        <Box>
+          <Typography>病人病历</Typography>
+        </Box>
       </Box>
     </Container>
-  )
+  );
 }
--- src/pages/signin/Patient.js
+++ src/pages/signin/Patient.js
@@ -3,11 +3,16 @@ import { useHistory, useLocation } from 'react-router-dom';
 import Button from '@material-ui/core/Button';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import TextField from '@material-ui/core/TextField';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
+import Link from '@material-ui/core/Link';
+import Grid from '@material-ui/core/Grid';
 import Box from '@material-ui/core/Box';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
 import PersonOutlineIcon from '@material-ui/icons/PersonOutline';
+import withWidth, { isWidthUp, isWidthDown } from '@material-ui/core/withWidth';
 import { ReactComponent as Icon } from '../../assets/images/icon.svg';
 
 const useStyles = makeStyles(theme => ({
@@ -33,34 +38,38 @@ const useStyles = makeStyles(theme => ({
   },
 }));
 
+
 export default function Patient() {
   const classes = useStyles();
   const history = useHistory();
-  const [name, setName] = useState('default value');
-  const [email, setEmail] = useState('default value');
+  const [myName, setName] = useState('default value');
+  const [myEmail, setEmail] = useState('default value');
   const [taboo, setTaboo] = useState('default value');
   const [allergies, setAllergies] = useState('default value');
   const [inputContent, setInputContent] = useState('');
-  const getInfo = async () => {
-    const response = await fetch('/api/account/getinfo', {
-      method: 'get',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(),
-    });
+   const getInfo = () => {
+    setName('testname');
+    setEmail('123456@zju.edu.cn')
+  }; 
+  
+  /* const getInfo = async () => {
+    const response = await fetch('/api/account/getinfo');
+    const body = await response.json();
     console.log(response);
     if (response.ok) {
-      const body = await response.json();
       console.log(body);
-      setName(body.firstName + body.lastName);
-      setEmail(body.email);
+      setName(body.lastName + body.firstName);
+      setEmail(body.myEmail);
     }
-  }
+  }; */
   const handleEditPWDButtonClick = event => {
     // （待完成）跳过验证界面直接进入修改密码的界面
     history.push({
       pathname: '/editpass',
+      state: {
+        email: myEmail,
+        homepage: true,
+      },
     });
   };
   const handleSaveInfoButtonClick = event => {
@@ -75,42 +84,74 @@ export default function Patient() {
     setInputContent(text);
     console.log(`Getting new text: ${text}`);
   };
-
   return (
-    <Container component="main" className={classes.page}>
+    <Container component="main" className={classes.page} onClick={getInfo}>
       <CssBaseline />
       <Box className={classes.start} bgcolor="#87CEFA" p={1}>
-        <Box><Icon /></Box>
-        <Box><PersonOutlineIcon /></Box>
-        <Box><Typography>个人资料</Typography></Box>
+        <Box>
+          <Icon />
+        </Box>
+        <Box>
+          <PersonOutlineIcon />
+        </Box>
+        <Box>
+          <Typography>个人资料</Typography>
+        </Box>
       </Box>
-      <Box className={classes.start} p={1} color="primary.main" borderBottom={5}>
+      <Box
+        className={classes.start}
+        p={1}
+        color="primary.main"
+        borderBottom={5}
+      >
         <Typography>基本信息</Typography>
       </Box>
-      <Box className={classes.center} p={1} borderBottom={5} borderColor="primary.main">
-        <Box display='flex' width='33%'>
-          <Typography>姓名：{name}</Typography>
+      <Box
+        className={classes.center}
+        p={1}
+        borderBottom={5}
+        borderColor="primary.main"
+      >
+        <Box display="flex" width="33%">
+          <Typography>姓名：{myName}</Typography>
         </Box>
-        <Box display='flex' width='33%'>
-          <Typography>邮箱：{email}</Typography>
+        <Box display="flex" width="33%">
+          <Typography>邮箱：{myEmail}</Typography>
         </Box>
-        <Box display='flex' width='33%'>
-          <Button variant="contained" color="primary" onClick={handleEditPWDButtonClick}>修改密码</Button>
+        <Box display="flex" width="33%">
+          <Button
+            variant="contained"
+            color="primary"
+            onClick={handleEditPWDButtonClick}
+          >
+            修改密码
+          </Button>
         </Box>
       </Box>
       <Box className={classes.center} p={1}>
-        <Box display='flex' width='33%'>
+        <Box display="flex" width="33%">
           <Typography>禁忌史：</Typography>
           <TextField defaultValue={taboo} onChange={handleInputChange} />
         </Box>
-        <Box display='flex' width='33%'>
+        <Box display="flex" width="33%">
           <Typography>过敏史：</Typography>
-          <TextField defaultValue={allergies} onChange={handleInputChange} id='taboo' />
+          <TextField
+            defaultValue={allergies}
+            onChange={handleInputChange}
+            id="taboo"
+          />
         </Box>
-        <Box display='flex' width='33%'>
-          <Button variant="contained" color="primary" onClick={handleSaveInfoButtonClick} id='allergies'>保存</Button>
+        <Box display="flex" width="33%">
+          <Button
+            variant="contained"
+            color="primary"
+            onClick={handleSaveInfoButtonClick}
+            id="allergies"
+          >
+            保存
+          </Button>
         </Box>
       </Box>
     </Container>
-  )
+  );
 }
