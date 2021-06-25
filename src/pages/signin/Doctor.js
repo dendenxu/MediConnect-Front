6c6f5a9ed37f69927d1bb4e96d52167145e8e0a1
BlossomWay <2485280111@qq.com>
2021-06-25 11:58:24 +0800

    feat：删除了一些没用到的包

--- src/pages/signin/Doctor.js
+++ src/pages/signin/Doctor.js
@@ -3,16 +3,11 @@ import { useHistory, useLocation } from 'react-router-dom';
 import Button from '@material-ui/core/Button';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import TextField from '@material-ui/core/TextField';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Checkbox from '@material-ui/core/Checkbox';
-import Link from '@material-ui/core/Link';
-import Grid from '@material-ui/core/Grid';
 import Box from '@material-ui/core/Box';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
 import PersonOutlineIcon from '@material-ui/icons/PersonOutline';
-import withWidth, { isWidthUp, isWidthDown } from '@material-ui/core/withWidth';
 import { ReactComponent as Icon } from '../../assets/images/icon.svg';
 
 const useStyles = makeStyles(theme => ({
